83b166852c6bb8abd5598b188ce1c7fb8350d4cd
XiongweiWu <XiongweiWu@users.noreply.github.com>
2021-05-22 17:12:46 +0800

    fix the bug in model init file

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -8,13 +8,12 @@ from .resnet import ResNet, ResNetV1c, ResNetV1d
 from .resnext import ResNeXt
 from .unet import UNet
 from .pvt import pvt_small, pvt_small_f4, pvt_tiny
-from .pvt_dia import pvt_small_dia
 from .vit import VisionTransformer
 from .vit_mla import VIT_MLA
 
 __all__ = [
     'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'FastSCNN',
     'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3', 
-    'pvt_small', 'pvt_small_f4', 'pvt_tiny', 'pvt_small_dia',
+    'pvt_small', 'pvt_small_f4', 'pvt_tiny', 
     'VisionTransformer', 'VIT_MLA'
 ]
